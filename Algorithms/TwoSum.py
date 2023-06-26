b41cf03002e635cb79ec7e66983392399349235a
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-06-26 15:04:06 -0400

    Two Sum question added

--- Algorithms/TwoSum.py
+++ Algorithms/TwoSum.py
@@ -1,3 +1,9 @@
+################### Question ########################
+
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
 ################### SOLUTION 1 ########################
 
 # # In this solution we'll start from the first entry and look for it's compliment in indexes after itself.
