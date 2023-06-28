77fbf6bc1a80d5237e3b223957be5a20d190c3dc
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-06-28 09:14:14 -0400

    Clean Code for Add Two Number #2

--- Algorithms/AddTwoNumbers.py
+++ Algorithms/AddTwoNumbers.py
@@ -141,3 +141,36 @@ class Solution:
                 node.next = new_node 
 
         return ans_head
+    
+################### SOLUTION 2 BUT BETTER ########################
+
+# This ones logic is the same as solution 2 but cleaner code
+# It's not confusing itself with different scenarios. But "makes" them all looking like one scenario
+# by adding imaginary 0s to the short number -- not literally adding.
+# when it comes to it and there is no more digit, adds it with 0!
+
+# class Solution:
+#     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+#         dummyHead = ListNode(0)
+#         tail = dummyHead
+#         carry = 0
+
+#         while l1 is not None or l2 is not None or carry != 0:
+#             digit1 = l1.val if l1 is not None else 0
+#             digit2 = l2.val if l2 is not None else 0
+
+#             sum = digit1 + digit2 + carry
+#             digit = sum % 10
+#             carry = sum // 10
+
+#             newNode = ListNode(digit)
+#             tail.next = newNode
+#             tail = tail.next
+
+#             l1 = l1.next if l1 is not None else None
+#             l2 = l2.next if l2 is not None else None
+
+#         result = dummyHead.next
+#         # Delete dummyHead
+#         dummyHead.next = None
+#         return result
