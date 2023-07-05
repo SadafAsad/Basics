e378c524bb5d345b3acaec51891f3e627dd19869
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-05 11:51:55 -0400

    Instruction for Median of Two Sorted Arrays #2

--- Algorithms/MedianofTwoSortedArrays.py
+++ Algorithms/MedianofTwoSortedArrays.py
@@ -36,3 +36,30 @@ class Solution:
         if len(sorted_array)%2==1:
             return sorted_array[len(sorted_array)//2]
         return (sorted_array[len(sorted_array)//2]+sorted_array[len(sorted_array)//2-1])/2
+    
+################### SOLUTION 2 ######################
+
+# Credits to LeetCode: (-- in my own words tho ;) --)
+# When you hear talking about finding something in sorted arrays, one things that should always pop in your head is binary search.
+# In this questions we are looking for the median in two sorted arrays. (after the two has been merged together)
+# Let's see what we can get if we compare their medians:
+# If A's median is smaller than B's median, then we can conclude that the left side of A would've been completely to the left side of
+# their merged array and B's right side would've been completely to the right side of their merged array. 
+# (left and right with respect to the median to be found)
+# So we can say number of(A left) elements would have been for sure in the left side of A+B 
+# and number of(B right) elements would have been for sure in the right side of A+B
+# Now let's see when we are talking about median what that actually means:
+# where should we look for the median of A+B? in the middle right? which means n(A+B)/2
+# (yeah, it differs when the number of elements is odd or even, but let's think about the concept for now)
+# so let's say the median index (middle index) of our target (our median) is k
+# This means we are looking for the k+1th element in the A+B (+1 is because k is index)
+# WELL HELLO! we can know at least how many elements will be to the left side and right side of A+B by comparing their medians!
+# That can help us to decide that k+1th element could possibly be to the left or right side!
+# If k+1 is greater than half of the A+B elements then we can stop looking for it in the left side of A!
+# Cause that side (the smaller side) will be for sure in the left side of A+B array and we know k+1th is not there
+# And when it's smaller or equal to the half of the A+B elemenets then we can stop looking in the right side of B!
+# Cause that side (the larger side) will be for sure in the right side of A+B array and we know k+1th is not there
+# Time Complexity = O(logm+logn) = O(logmn)
+# Space Complexity = O(logmn) <- only because of recursion otherwise we are allocating more than constant number of space
+
+
