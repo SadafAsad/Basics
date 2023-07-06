bc3022a717c442eac09287122ee86316adc45272
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-06 10:45:29 -0400

    Longest Palindromic Substring #1

--- Algorithms/LongestPalindromicSubstring.py
+++ Algorithms/LongestPalindromicSubstring.py
@@ -1,34 +1,38 @@
+################### Question ########################
+
+# Given a string s, return the longest palindromic substring in s.
+
+################### SOLUTION 1 ######################
+# Credits to LeetCode
+# This is a brute force solution and we are checking all the subsrtings to see if we find any palindromic ones.
+# We'll start checking with the longest substrings so then as soon as one is found we can returned it.
+# Using the function 'check', we'll give it start and end index of the substring to check for it being palindromic or not
+# Time Complexity = O(n^3)
+# Space Complexity = O(1)
+
 class Solution:
-    def longestPalindrome(self, s:str) -> str:
-        max_tmp = ""
-        start_index_tmp = 0
-        while start_index_tmp<len(s):
-            max_s = ""
-            start_index = start_index_tmp
-            end_index = len(s)-1
-            counter = 0
-            while start_index<end_index:
-                if s[start_index]==s[end_index]:
-                    max_s+=s[start_index]
-                    start_index+=1
-                    end_index-=1
-                else:
-                    counter+=1
-                    max_s = ""
-                    start_index = start_index_tmp
-                    end_index = len(s)-1-counter
-            
-            odd = False
-            if start_index==end_index:
-                odd = True
-                max_s+=s[start_index]
-            
-            i = len(max_s)-2 if odd else len(max_s)-1
-            while i>=0:
-                max_s+=max_s[i]
-                i-=1
-            
-            if len(max_tmp)<len(max_s):
-                max_tmp = max_s
-            start_index_tmp+=1
-        return max_tmp
+    def longestPalindrome(self, s: str) -> str:
+        def check(i, j):
+            left = i
+            right = j - 1
+            while left < right:
+                if s[left] != s[right]:
+                    return False
+                
+                left += 1
+                right -= 1
+            return True
+        
+        # checking substrings with different length, starting from the longest ones, len(s)
+        for length in range(len(s), 0, -1):
+            # We are basically moving the start index based on the length of the substring
+            # first time we are looking for len(s) length substrings which is only one so start will only be 0
+            # second round we are looking for len(s)-1 lengths which means there will be two substrings with that length
+            # [0:len(s)] and [1:len(s)+1] and so on
+            for start in range(len(s) - length + 1):
+                # As soon as the check function returns True the first time
+                # That will be the longest palindromic substring
+                # The rest found after that will be shorter
+                if check(start, start + length):
+                    return s[start:start + length]
+        return ""
