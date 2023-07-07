7e7ad9ca50fa60329c7907798800bc65161b0e1b
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-07 12:31:11 -0400

    Reverse Integer #1

--- Algorithms/ReverseInteger.py
+++ Algorithms/ReverseInteger.py
@@ -5,3 +5,19 @@
 # Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
 ################### SOLUTION 1 ######################
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        positive = True
+        if x<0:
+            positive = False
+            x*=-1
+        ans = 0
+        while x!=0:
+            ans = ans*10+x%10
+            x = x//10
+        if positive and ans<2**31:
+            return ans  
+        elif ~positive and ans<2**31+1:
+            return ans*-1
+        return 0
