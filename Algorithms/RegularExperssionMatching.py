8ac61731732b44876ea98da445e67b6cb46c939d
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-08 11:27:15 -0400

    Constraints added

--- Algorithms/RegularExperssionMatching.py
+++ Algorithms/RegularExperssionMatching.py
@@ -5,4 +5,11 @@
 # '*' Matches zero or more of the preceding element.
 # The matching should cover the entire input string (not partial).
 
+# Constraints:
+# 1 <= s.length <= 20
+# 1 <= p.length <= 20
+# s contains only lowercase English letters.
+# p contains only lowercase English letters, '.', and '*'.
+# It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+
 ################### SOLUTION 1 ########################
