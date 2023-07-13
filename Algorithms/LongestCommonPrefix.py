c6bac3e794f14b22fcedf449beda3e3e23773424
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-13 17:21:35 -0400

    Longest Common Prefix #1

--- Algorithms/LongestCommonPrefix.py
+++ Algorithms/LongestCommonPrefix.py
@@ -6,8 +6,28 @@
 
 ################### SOLUTION 1 ########################
 
+# The whole idea behind this solution is very simple:
+# We're gonna check each character of each string in the array
+# So we'll first get the min length of the existing strings in the array 
+# Then we'll need two loops, one to iterate over the characters and the other to iterate over the array of strings
+# Time Complexity = O(n^2)
+# Space Complexity = O(1)
+
 from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pass
+        min_len = float('inf')
+        for str in strs:
+            min_len = min(min_len, len(str))
+
+        prefix = ""
+        char_index = 0
+        while char_index<min_len:
+            for i in range(len(strs)):
+                if strs[0][char_index]!=strs[i][char_index]:
+                    return prefix
+            prefix+=strs[0][char_index]
+            char_index+=1
+        
+        return prefix
