460b45e07683dd17d10e5af19f19f41320619a53
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-13 12:12:49 -0400

    Container With Most Water #1 -- Time Limit Exceeded

--- Algorithms/ContainerWithMostWater.py
+++ Algorithms/ContainerWithMostWater.py
@@ -2,4 +2,10 @@ from typing import List
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        pass
+        ans = 0
+        for x in range(len(height), 0, -1):
+            for y_index in range(len(height)-x):
+                storage = x*min(height[y_index], height[y_index+x])
+                if storage>=ans:
+                    ans = storage
+        return ans
