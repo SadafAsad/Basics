09c533327c4f34d8dad2473e6fc0f28a1bd9381b
sdf <34832185+SadafAsad@users.noreply.github.com>
2023-07-13 12:15:43 -0400

    Container With Most Water #1 -- Variable Name Change

--- Algorithms/ContainerWithMostWater.py
+++ Algorithms/ContainerWithMostWater.py
@@ -19,7 +19,7 @@ class Solution:
         ans = 0
         for x in range(len(height), 0, -1):
             for y_index in range(len(height)-x):
-                storage = x*min(height[y_index], height[y_index+x])
-                if storage>=ans:
-                    ans = storage
+                container = x*min(height[y_index], height[y_index+x])
+                if container>=ans:
+                    ans = container
         return ans
